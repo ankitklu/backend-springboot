49e248c54a49b12c41b57223247e00bf5b0ad888
ankitklu <ankit.klu.2022@gmail.com>
2024-10-23 23:44:25 +0530

    Create and Read operation done

---
+++ src/main/java/com/example/fullstack_backend/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.fullstack_backend.controller;
+
+import com.example.fullstack_backend.model.User;
+import com.example.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+
+}
